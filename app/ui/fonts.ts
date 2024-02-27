60cd7f098d1d34f11c8841edd3671ee7c12e0416
Duan Liwen <github@relay.lwdmail.com>
2024-02-27 10:49:59 +0800

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Merriweather_Sans, Merriweather, Lusitana } from 'next/font/google';
+
+export const merriweatherSans = Merriweather_Sans({ subsets: ['latin'] });
+export const merriweather = Merriweather({ weight: '400', subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
